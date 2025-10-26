aff05daa1f881a8801c5fdd7747f86756fa1fde8
Vinny Guminski <vincentguminski@gmail.com>
2025-10-25 18:24:05 -0700

    Initial Spring Boot commit

---
+++ src/main/java/edu/msu/demo/HelloController.java
@@ -0,0 +1,14 @@
+package edu.msu.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello from Vinny!";
+    }
+}
+
---
+++ src/test/java/edu/msu/demo/DemoVinnyApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.msu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoVinnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
